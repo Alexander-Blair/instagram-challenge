414a037f0f48b2d8fb2e03dfc930b80637e180d4
Alexander Blair <zanderb@hotmail.co.uk>
2017-08-28 23:24:24 +0100

    User can dislike a post (#11)

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,8 +1,23 @@
 module LikesHelper
   def like_src(post, user)
+    return "assets/empty-like.png" unless liked_post(post, user)
+    "assets/red-like.png"
+  end
+
+  def liked_post(post, user)
     post.likes.each do |like|
-      return 'assets/red-like.png' if like.liked_by(user)
+      return like.id if like.liked_by(user)
     end
-    'assets/empty-like.png'
+    return false
+  end
+
+  def like_action(post, user)
+    return likes_path unless liked_post(post, user)
+    like_path(liked_post(post, user))
+  end
+
+  def like_method(post, user)
+    return :create unless liked_post(post, user)
+    :delete
   end
 end
